bdee201ec5a55684945c22d0bfdbc157054bf6ba
jeremyfukunaga <jfukunaga981@gmail.com>
2023-06-23 20:59:42 -0700

    Create main.py

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ def main():
     ##################################################
     pass
 
+    # Original string
+    original_str = "Python Programming"
+
+    # Extract "Python" from the original string
+    sub1 = original_str[0:6]
+
+    # Extract "Programming" from the original string
+    sub2 = original_str[7:]
+
+    # Merge the two substrings using string concatenation
+    merged_str = sub2 + " " + sub1
+
+    # Print the merged string
+    print(merged_str)
+
+
 
 if __name__ == '__main__':
     main()
