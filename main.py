2b962d7060ee071c230f4a8fab196a06a99ad97b
Kyuwoong Lee <kwooler@gmail.com>
2023-06-24 22:21:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+    # pass
 
+    # Original string
+    original_str = "Python Programming"
+
+    # Extract "Python" from the original string
+    sub1 = original_str[0:6]
+
+    # Extract "Programming" from the original string
+    sub2 = original_str[7:]
+
+    # Merge the two substrings using string concatenation
+    merged_str = sub2 + " " + sub1
+
+    # Print the merged string
+    print(sub2)
+    print(sub1)
+    print(merged_str)
+    return sub2, sub1, merged_str
 
 if __name__ == '__main__':
     main()
